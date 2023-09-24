556d52972a6477ec8772e8df5b433b1d4ebb78d0
M. Soban Hussain <captaindeath740@gmail.com>
2023-09-24 16:27:53 +0500

    Alien Colors #1

--- Assign-25.ts
+++ Assign-25.ts
@@ -6,6 +6,6 @@ if(alien_color1.green){
 
 console.log("An alien is just shot down.");
 enum alien_color2 {"red","green","yellow"};
-if(alien_color2.green){
+if(alien_color2.red){
     console.log("You have earned 5 points.")
 }
