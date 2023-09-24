37e42b77a0526d3eefd82b7ec918263aaa8e65c7
M. Soban Hussain <captaindeath740@gmail.com>
2023-09-24 17:10:53 +0500

    Alien Colors #3--- Part (c)

---
+++ Assign-27(c).ts
@@ -0,0 +1,11 @@
+console.log("An alien is just shot down.");
+let alien_color = "red";
+if(alien_color == "green"){
+    console.log("You have earned 5 points.")
+}
+else if(alien_color == "yellow"){
+    console.log("You have earned 10 points.")
+}
+else if(alien_color == "red"){
+    console.log("You have earned 15 points.")
+}
